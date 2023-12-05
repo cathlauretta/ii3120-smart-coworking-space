2568fc6d87009301d7f6e135517d6751a7b1e11d
nauravalda <16521058@mahasiswa.itb.ac.id>
2023-12-05 10:27:11 +0700

    callback, .env

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -11,10 +11,10 @@ export const createClient = (request: NextRequest) => {
   });
 
   const supabase = createServerClient<Database>(
-    "https://nkszakyxtqmimbufxqdo.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rc3pha3l4dHFtaW1idWZ4cWRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE3MDAwMTUsImV4cCI6MjAxNzI3NjAxNX0.AKLggS9uc8MqyqpJPXGKaSlOh6RuHY8ANHmn3wmnesE",
-    // process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    // process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    // "https://nkszakyxtqmimbufxqdo.supabase.co",
+    // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rc3pha3l4dHFtaW1idWZ4cWRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE3MDAwMTUsImV4cCI6MjAxNzI3NjAxNX0.AKLggS9uc8MqyqpJPXGKaSlOh6RuHY8ANHmn3wmnesE",
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
