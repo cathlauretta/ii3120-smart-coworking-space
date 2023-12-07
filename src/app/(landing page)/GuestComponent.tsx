56e161ee31a30766079deed9c9bbc4920227f507
ojan <16521411@mahasiswa.itb.ac.id>
2023-12-07 19:40:57 +0700

    sy pindah ke file baru yg sign in

--- src/app/(landing page)/GuestComponent.tsx
+++ src/app/(landing page)/GuestComponent.tsx
@@ -1,41 +1,48 @@
 import Image from 'next/image'
 import {
-  Button
+  Button,
+  Input,
 } from "@chakra-ui/react"
 import { Brand } from "@/components/brand"
 import {
   FaGoogle,
   FaFacebook,
 } from "react-icons/fa"
+import SignUpForm from '../signup/SignUpComponent'
 
 export default function GuestHome() {
   return (
     // pink section
     <div className='flex px-[150px] py-[100px] gap-[50px] bg-[#FBB6CE]'>
-      <div className='flex  flex-col w-full bg-white gap-10 px-24 py-14 rounded-lg'>
-        <div className='flex flex-row items-center'>
-          <Brand />
-          <div className="text-gray-900 text-4xl font-extrabold font-['Inter'] leading-10">, Coolest Workspace Ever!</div>
+      {/* Content */}
+      {/* left side */}
+      <div className='flex flex-col w-full h-full bg-white content-evenly px-12 py-14 rounded-lg gap-10 z-1'>
+        <div className='flex flex-col w-full gap-5'>
+          <div className='flex flex-row items-center'>
+            <Brand />
+            <div className="felx w-full text-gray-900 text-4xl font-extrabold font-['Inter'] leading-10">, Coolest Workspace Ever!</div>
+          </div>
+          <div className="w-full text-gray-900 text-base font-normal font-['Inter'] leading-normal">
+            Lelah dan ingin menemukan suasana kerja dan belajar baru agar harimu lebih sejuk? Ayo bergabung dengan kami menjadi bagian dari Sentrice Member untuk bisa merasakan sensasi belajar dan bekerja yang nyaman!
+          </div>
         </div>
+        <div className='flex flex-row w-full h-56 gap-5'>
+          <img className="w-full h-56 object-cover rounded-lg" src="/assets/facility/cowork-1.jpg" />
+          <img className="w-full h-56 object-cover rounded-lg" src="/assets/facility/cowork-2.jpg" />
+          <img className="w-full h-56 object-cover rounded-lg" src="/assets/facility/cowork-3.jpg" />
+        </div>
+        <Button colorScheme='purple' size="lg" width="30%">
+          Lihat lainnya
+        </Button>
       </div>
+
+      {/* sign up */}
       <div className='flex flex-col w-[1100px] bg-white gap-10 px-24 py-14 rounded-lg'>
-        <div className='flex flex-col w-full justify-start items-center gap-2.5 inline-flex'>
-          <div className="text-gray-900 text-4xl font-extrabold font-['Inter'] leading-10">Join Us Now!</div>
-          <div className="w-96 text-center text-gray-900 text-base font-semibold font-['Inter'] leading-normal">Bergabung menjadi bagian dari Sentrice Member!<br />Pilih metode sign up:</div>
-        </div>
-        <div className='flex w-full gap-5'>
-          <Button leftIcon={<FaGoogle />} colorScheme='purple' variant='outline' size="lg" width="100%">
-            Google
-          </Button>
-          <Button leftIcon={<FaFacebook />} colorScheme='purple' variant='outline' size="lg" width="100%">
-            Facebook
-          </Button>
-        </div>
-        <div className="w-full h-fit justify-center items-center gap-2.5 inline-flex">
-          <div className="grow shrink basis-0 h-px border border-slate-400"></div>
-          <div className="text-center text-slate-400 text-sm font-semibold font-['Inter'] leading-tight">atau lanjut menggunakan email</div>
-          <div className="grow shrink basis-0 h-px border border-slate-400"></div>
-        </div>
+        <SignUpForm />
+      </div>
+      {/* Background */}
+      <div>
+        asd
       </div>
     </div>
   )
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,19 +1,12 @@
 import React from "react";
 import {
     Flex,
-    Button,
     Image,
-    InputGroup,
-    Input,
-    InputLeftElement,
     Text,
-    Divider,
-    AbsoluteCenter,
     Link,
 } from "@chakra-ui/react";
 import { Brand } from "@/components/brand";
-import { Password } from "@/components/Password";
-import { FaEnvelope, FaEdit, FaGoogle, FaFacebook } from "react-icons/fa";
+import SignUpForm from "@/app/signup/SignUpComponent";
 
 const desc =
     "Bergabung menjadi bagian dari Centrice Member!\nPilih metode Sign Up:";
@@ -32,109 +25,8 @@ export default function LoginPage() {
                 gap="80px">
                 <Brand />
 
-                {/* Login */}
-                <Flex
-                    flexDir={"column"}
-                    justifyContent={"center"}
-                    alignItems={"center"}
-                    gap="40px"
-                >
-                    <Flex
-                        flexDir={"column"}
-                        justifyContent={"center"}
-                        alignItems={"center"}
-                        textColor={DEFAULT_TEXT_COLOR}
-                        gap={"10px"}>
-                        <Text fontSize="36px" fontWeight="800">
-                            Create an Account
-                        </Text>
-                        <Text
-                            fontSize="16px"
-                            fontWeight="600"
-                            fontStyle="inter"
-                            whiteSpace="pre-line"
-                            textAlign="center">
-                            {desc}
-                        </Text>
-                    </Flex>
-
-                    {/* Google & Facebook */}
-                    <Flex gap="20px" w="100%">
-                        <Button
-                            leftIcon={<FaGoogle />}
-                            colorScheme="purple"
-                            variant="outline"
-                            size="lg"
-                            width="100%">
-                            Google
-                        </Button>
-                        <Button
-                            leftIcon={<FaFacebook />}
-                            colorScheme="purple"
-                            variant="outline"
-                            size="lg"
-                            width="100%">
-                            Facebook
-                        </Button>
-                    </Flex>
-
-                    {/* Input Groups */}
-                    <Flex
-                        w="100%"
-                        flexDir={"column"}
-                        justifyContent={"center"}
-                        alignItems={"center"}
-                        gap="20px"
-                        textColor={DEFAULT_TEXT_COLOR}>
-                        {/* Divider */}
-                        <Flex w="100%" position="relative">
-                            <Divider />
-                            <AbsoluteCenter
-                                px="1"
-                                fontWeight="600"
-                                bg="white"
-                                w="68%"
-                                textAlign="center"
-                                textColor="gray.400">
-                                atau lanjut menggunakan email
-                            </AbsoluteCenter>
-                        </Flex>
-
-                        {/* Email */}
-                        <Flex flexDir="column" gap="10px" w="100%">
-                            <InputGroup>
-                                <InputLeftElement
-                                    color="gray.400"
-                                    pointerEvents="none">
-                                    <FaEdit />
-                                </InputLeftElement>
-                                <Input
-                                    type="text"
-                                    placeholder="Full Name"
-                                    pl="40px"
-                                />
-                            </InputGroup>
-                            <InputGroup>
-                                <InputLeftElement
-                                    color="gray.400"
-                                    pointerEvents="none">
-                                    <FaEnvelope />
-                                </InputLeftElement>
-                                <Input
-                                    type="email"
-                                    placeholder="Email"
-                                    pl="40px"
-                                />
-                            </InputGroup>
-                            <Password />
-                        </Flex>
-                    </Flex>
-
-                    {/* Sign Up Button */}
-                    <Button w="100%" bg="purple.500" textColor="white">
-                        Sign Up
-                    </Button>
-                </Flex>
+                {/* Sign Up */}
+                <SignUpForm />
 
                 {/* Link to Login */}
                 <Flex flexDir={"row"} textColor="purple.800" gap="10px">
